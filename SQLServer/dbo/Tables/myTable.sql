843694a955484db1313512084d2f730882dd4c06
spry <yoshida@spr.jp>
2016-04-06 23:27:24 +0900

    change message from update3.

---
+++ SQLServer/dbo/Tables/myTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[myTable] (
+    [Id]    INT           NOT NULL,
+    [Name]  NVARCHAR (50) NOT NULL,
+    [Value] NVARCHAR (50) NULL,
+    [Address] NVARCHAR(50) NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
